667338189c0f81c8aa305061796bc2e9b46694ae
Leonardo Castorina <leonardo.castorina98@gmail.com>
2021-08-08 20:36:57 +0100

    Add code for pymol figures

---
+++ pymol_images.py
@@ -0,0 +1,40 @@
+import pymol
+import pandas as pd
+import numpy
+import urllib
+from pathlib import Path
+import numpy as np
+
+import matplotlib as mpl
+mpl.rc("savefig", dpi=300)
+import matplotlib.pyplot as plt
+plt.style.use("ggplot")
+plt.rcParams["figure.figsize"] = (10, 10)
+
+model_name = "skilled-wind-134-timed_hs1000_l2_dp03_weighted"
+# For loop does not work - ffs pymol
+pdb_structures = ["4fcg", "3e3v", "2v3i"]
+pdb_name = pdb_structures[2]
+original = f"{pdb_name}.pdb.gz"
+predicted = f"af2/{model_name}/{pdb_name}A_unrelaxed_model_1.pdb"
+pymol.pymol_argv = ['pymol', '-qc']
+pymol.finish_launching()
+cmd = pymol.cmd
+cmd.delete('all')
+cmd.load(original)
+cmd.load(predicted)
+print(cmd.get_object_list('all'))
+sel1, sel2 = cmd.get_object_list('all')
+# Color white (original) cyan (prediction)
+cmd.color("white", sel1)
+cmd.color("cyan", sel2)
+# Align structures
+cmd.align(sel1, sel2)
+# Hide waters
+cmd.select(name="h2o", selection="solvent")
+cmd.hide(selection="h2o")
+# Hide 3D shadows
+cmd.set("ray_shadows", value="off")
+# Export HD
+cmd.ray(2400, 2400)
+cmd.png(f"af2/{model_name}_{pdb_name}.png", width=2400, height=2400, dpi=300, ray=1)
